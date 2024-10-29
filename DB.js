28d8d67710aa69e9f8a7747434876fe3e503542a
RacheliShayzada <054racheli@gmail.com>
2024-10-29 13:46:53 +0200

    after change the id to string

--- DB.js
+++ DB.js
@@ -1,11 +1,11 @@
 let cards = [    
-    { id: 1, color: 'red', text: 'Hello world!'},
-    { id: 2, color: 'green', text: 'This is a test'},
-    { id: 3, color: 'blue', text: 'This is a sample text'},
-    { id: 4, color: 'yellow', text: 'This is a sample text '},
-    { id: 5, color: 'orange', text: 'This is a sample text'},
-    { id: 6, color: 'purple', text: 'This is a sample  text '},
-    { id: 7, color: 'pink', text: 'This is a sample text'}
+    { id: '1', color: 'red', text: 'Hello world!'},
+    { id: '2', color: 'green', text: 'This is a test'},
+    { id: '3', color: 'blue', text: 'This is a sample text'},
+    { id: '4', color: 'yellow', text: 'This is a sample text '},
+    { id: '5', color: 'orange', text: 'This is a sample text'},
+    { id: '6', color: 'purple', text: 'This is a sample  text '},
+    { id: '7', color: 'pink', text: 'This is a sample text'}
 ];
 
 export default cards;
