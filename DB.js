99d767b744ed123162f546e0e70925d87bb2a9da
RacheliShayzada <054racheli@gmail.com>
2024-10-29 15:15:00 +0200

    after changing the colors on the db

--- DB.js
+++ DB.js
@@ -1,11 +1,11 @@
 let cards = [    
-    { id: '1', color: 'red', text: 'Hello world!'},
-    { id: '2', color: 'green', text: 'This is a test'},
-    { id: '3', color: 'blue', text: 'This is a sample text'},
-    { id: '4', color: 'yellow', text: 'This is a sample text '},
-    { id: '5', color: 'orange', text: 'This is a sample text'},
-    { id: '6', color: 'purple', text: 'This is a sample  text '},
-    { id: '7', color: 'pink', text: 'This is a sample text'}
+    { id: '1', color: 'lightcoral', text: 'Hello world!'},
+    { id: '2', color: '#FFA500', text: 'This is a test'},
+    { id: '3', color: '#00BFFF', text: 'This is a sample text'},
+    { id: '4', color: '#9370DB', text: 'This is a sample text '},
+    { id: '5', color: 'khaki', text: 'This is a sample text'},
+    { id: '6', color: 'pink', text: 'This is a sample  text '},
+    { id: '7', color: 'lightseagreen', text: 'This is a sample text'}
 ];
 
 export default cards;
